f03275c116fe2207e34e0c0da46f02237dfbab14
Amir <amir.hakim@kolu.web.id>
2020-04-11 23:05:03 +0700

    Rename

--- test/example_flex_message.php
+++ test/example_flex_message.php
@@ -1,11 +1,11 @@
 <?php
 
-$json = file_get_contents('../flex/flex_nasional.json');
+$json = file_get_contents('../templates/flex_nasional.json');
 
 $message = json_decode($json);
 
 print_r($message);
 
-require_once "../flex/flex_nasional.php";
+require_once "../templates/flex_nasional.php";
 
 print_r(json_encode($flex));
