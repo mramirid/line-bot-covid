685102b6898c1ce36af3e2d9150a1b7b1374ddef
Sunu Ilham Pradika <sunudika@gmail.com>
2020-04-11 11:48:22 +0700

    Add modified db

--- assets/line_bot_covid_v1.sql
+++ assets/line_bot_covid_v1.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 11, 2020 at 04:47 AM
+-- Generation Time: Apr 10, 2020 at 10:07 AM
 -- Server version: 5.7.24
--- PHP Version: 7.2.19
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,19 +25,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `detail_pengambilan_provinsi`
+-- Table structure for table `internasional`
 --
 
-CREATE TABLE `detail_pengambilan_provinsi` (
+CREATE TABLE `internasional` (
   `id` int(11) NOT NULL,
-  `id_pengambilan_provinsi` int(11) NOT NULL,
-  `kode_provinsi` int(11) NOT NULL,
-  `nama_provinsi` varchar(50) NOT NULL,
   `positif` int(11) NOT NULL,
   `sembuh` int(11) NOT NULL,
-  `dalam_perawatan` int(11) NOT NULL,
-  `meninggal` int(11) NOT NULL
+  `meninggal` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `internasional`
+--
+
+INSERT INTO `internasional` (`id`, `positif`, `sembuh`, `meninggal`, `created_at`, `updated_at`) VALUES
+(3, 1605548, 356161, 95808, '2020-04-10 16:35:40', '2020-04-10 16:36:21');
 
 -- --------------------------------------------------------
 
@@ -50,31 +55,27 @@ CREATE TABLE `nasional` (
   `positif` int(11) NOT NULL,
   `sembuh` int(11) NOT NULL,
   `meninggal` int(11) NOT NULL,
-  `dalam_perawatan` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `pengambilan_provinsi`
+-- Dumping data for table `nasional`
 --
 
-CREATE TABLE `pengambilan_provinsi` (
-  `id` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `nasional` (`id`, `positif`, `sembuh`, `meninggal`, `created_at`, `updated_at`) VALUES
+(3, 3293, 252, 280, '2020-04-08 15:48:04', '2020-04-10 15:48:04'),
+(4, 3512, 282, 306, '2020-04-09 15:51:28', '2020-04-10 16:04:14'),
+(6, 3512, 282, 306, '2020-04-10 17:05:03', '2020-04-10 17:05:27');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `detail_pengambilan_provinsi`
+-- Indexes for table `internasional`
 --
-ALTER TABLE `detail_pengambilan_provinsi`
+ALTER TABLE `internasional`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -84,32 +85,20 @@ ALTER TABLE `nasional`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `pengambilan_provinsi`
---
-ALTER TABLE `pengambilan_provinsi`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `detail_pengambilan_provinsi`
+-- AUTO_INCREMENT for table `internasional`
 --
-ALTER TABLE `detail_pengambilan_provinsi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `internasional`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `nasional`
 --
 ALTER TABLE `nasional`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pengambilan_provinsi`
---
-ALTER TABLE `pengambilan_provinsi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
