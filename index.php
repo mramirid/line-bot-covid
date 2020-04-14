87ef377fd9d402e475d4b9e6808d3dd1f6a8428e
Sunu Ilham Pradika <sunudika@gmail.com>
2020-04-14 11:37:23 +0700

    Delete unused message for user

--- index.php
+++ index.php
@@ -93,8 +93,7 @@ $app->post('/', function ($request, $response)
 			case '/help':
 				$message = '1. halo -> Respon halo' . PHP_EOL;
 				$message .= '2. /nasional -> Kasus COVID-19 di Indonesia'  . PHP_EOL;
-				$message .= '3. /provinsi -> List kasus seluruh provinsi' . PHP_EOL;
-				$message .= '4. /cari [nama_provinsi] -> mencari provinsi berdasarkan nama' . PHP_EOL;
+				$message .= '3. /cari [nama_provinsi] -> Mencari kasus provinsi berdasarkan nama' . PHP_EOL;
 				$message .= 'Misal: /cari jawa timur' . PHP_EOL;
 				// $message .= '4. /available_provinsi -> List provinsi yang datanya tersedia'  . PHP_EOL;
 				// $message .= '5. /cari_provinsi [kode_provinsi] -> Cari provinsi berdasarkan kode provinsi (lihat di /available_provinsi)';
